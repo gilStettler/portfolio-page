012d9b08046e10904bfc4debdb53b978cff173cf
Gil Stettler <gil.stettler@students.bfh.ch>
2024-09-27 17:40:54 +0200

    Versuch API Zen Quotes

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TileComponent } from '../tile/tile.component';
+import { QuoteService } from '../quote.service';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [TileComponent],
+  imports: [TileComponent, CommonModule],
   template: `
     <div class="homecontent">
       <div class="leftside">
@@ -23,8 +25,28 @@ import { TileComponent } from '../tile/tile.component';
         <app-tile label="Hobbies" color="#DD761C"></app-tile>
         <app-tile label="Random" color="#6dc5d1"></app-tile>
       </div>
+      <p>{{ quote.q }}</p>
     </div>
   `,
   styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {}
+export class HomepageComponent implements OnInit {
+  quote: { q: string; a: string; h: string } = { q: '', a: '', h: '' }; // Initialize with an empty quote
+
+  constructor(private quoteService: QuoteService) {}
+
+  ngOnInit(): void {
+    this.fetchQuote();
+  }
+
+  fetchQuote() {
+    this.quoteService.getRandomQuote().subscribe(
+      (response: { q: string; a: string; h: string }[]) => {
+        this.quote = response[0];
+      },
+      (error) => {
+        console.error('Error fetching quote:', error);
+      }
+    );
+  }
+}
---
+++ src/app/quote.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Quote {
+  q: string;
+  a: string;
+  h: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuoteService {
+  constructor(private http: HttpClient) {}
+
+  getRandomQuote(): Observable<any> {
+    return this.http.get<any>('https://zenquotes.io/api/random');
+  }
+}
