b54ee336f3caa3b9faa4f9f25b7e661278680c7e
Gil Stettler <gil.stettler@students.bfh.ch>
2024-10-20 13:03:37 +0200

    hinzufügen neuer environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
+    production: true,
     catApiKey: 'live_aOd70uiHEqn1XZXnG38QrjDvp3cEQ3v277YS6Y6b5POn2ANceyEIz1cJbP0gXHiw'  // Setze hier deinen API-Schlüssel für die Produktionsumgebung ein
   };
